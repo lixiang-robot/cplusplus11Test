aeb2c27f41b88d2fd4a654778d03f576b718c7cf
Xiang Li <xiangli.sky@gmail.com>
2016-08-05 09:28:30 -0700

    Added Atomic example

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
 #include <QCoreApplication>
 
+#include <iostream>
+#include <atomic>
+
+using namespace std;
+
+atomic<int> x = ATOMIC_VAR_INIT(false);
+atomic<int> y = ATOMIC_VAR_INIT(false);
+atomic<int> z = ATOMIC_VAR_INIT(0);
+
+void write_x()
+{
+    x.store(true, memory_order_seq_cst);
+}
+
+void write_y()
+{
+    y.store(true, memory_order_seq_cst);
+}
+
+void read_x_then_y()
+{
+    while (!x.load(std::memory_order_seq_cst))
+        ;
+    if (y.load(std::memory_order_seq_cst)) {
+        ++z;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    cout << "Hello World!" << endl;
 
     return a.exec();
 }
